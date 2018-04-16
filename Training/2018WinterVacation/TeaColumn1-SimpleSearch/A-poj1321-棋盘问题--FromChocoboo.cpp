0c3661a99aeb57294efd038d2102036c1498faea
Chocoboo <34242441+MoogleAndChocobo@users.noreply.github.com>
2018-04-16 12:58:34 +0800

    Update A-poj1321-棋盘问题--FromChocoboo.cpp

--- Training/2018WinterVacation/TeaColumn1-SimpleSearch/A-poj1321-棋盘问题--FromChocoboo.cpp
+++ Training/2018WinterVacation/TeaColumn1-SimpleSearch/A-poj1321-棋盘问题--FromChocoboo.cpp
@@ -16,25 +16,25 @@ typedef long long LL;
 typedef unsigned long long ULL;
 typedef long double LF;
 
-#define fli				freopen("in.txt", "r", stdin);
-#define flo				freopen("out.txt", "w", stdout);
-#define sync 			ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
-#define	rep(i, a, b)	for(int i = a; i <= b; i++)
-#define dow(i, a, b)	for(int i = a; i >= b; i--)
-#define mem(a)			memset(a, 0, sizeof(a))
-#define mst(a, b)		memset(a, b, sizeof(a))
-#define sfi(a)			scanf("%d", &a)
-#define sfs(a)			scanf("%s", a)
-#define sfl(a)			scanf("%lld", &a)
-#define sfd(a)			scanf("%lf", &a)
-#define pb(a)			push_back(a)
-#define all(c)			(c).begin(),(c).end()
-#define YES				puts("YES")
-#define NO				puts("NO")
-#define yes				puts("yes")
-#define no				puts("no")
-#define Yes				puts("Yes")
-#define No				pus("No")
+#define fli freopen("in.txt", "r", stdin);
+#define flo freopen("out.txt", "w", stdout);
+#define sync ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+#define	rep(i, a, b) for(int i = a; i <= b; i++)
+#define dow(i, a, b) for(int i = a; i >= b; i--)
+#define mem(a) memset(a, 0, sizeof(a))
+#define mst(a, b) memset(a, b, sizeof(a))
+#define sfi(a) scanf("%d", &a)
+#define sfs(a) scanf("%s", a)
+#define sfl(a) scanf("%lld", &a)
+#define sfd(a) scanf("%lf", &a)
+#define pb(a) push_back(a)
+#define all(c) (c).begin(),(c).end()
+#define YES puts("YES")
+#define NO puts("NO")
+#define yes puts("yes")
+#define no puts("no")
+#define Yes puts("Yes")
+#define No pus("No")
 
 const int MAX = 1e3 + 10;
 const int N = 16000;
